fe9494beffe70b4b7416db025300850dc0479101
NikolaJelic <jelicnikola37@gmail.com>
2021-07-07 21:53:03 +0200

    configure ctest

--- test/pini_test.cpp
+++ test/pini_test.cpp
@@ -5,13 +5,8 @@
 int main() {
 	pn::pini pin;
 
-	// std::string_view raw_input{"a=5\nb=10\n23=c"};
-	std::filesystem::path filename{"test/test.ini"};
-
-	std::cout << std::boolalpha << pin.load_file(filename) << "\n";
-	std::cout << "attack: " << pin.get_uint32("attack ") << "\n";
-	std::cout << "def " << pin.get_uint64("def ") << "\n";
-	std::cout << "health " << pin.get_int64("health ") << "\n";
-	std::cout << "def " << pin.get_double("def ") << "\n";
-	std::cout << "sp " << pin.get_int32("skill points ") << "\n";
+	std::string_view raw_input{"a=5\nb=10\n23=c"};
+	// std::filesystem::path filename{"/test/test.ini"};
+	if (!pin.load_text(raw_input)) { return 1; }
+	if (pin.get_int32("a") != 5) { return 1; }
 }
